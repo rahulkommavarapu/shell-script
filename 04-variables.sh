854fe0f25572107b36cab5b98cc5c23847943c49
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-01-06 15:24:09 +0530

    arguments

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+
+echo "$PERSON1::Hi $PERSON2"
+echo "$PERSON2::Hello $PERSON1!,How are you?"
+echo "$PERSON2::Hello $PERSON3,How are you Dear...!"
+echo "$PERSON3::iam fine $PERSON1,$PERSON2 What are you doing both of you Dears"
