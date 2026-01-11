93835f2f4554f9ea04c696b53562265b660cd4a2
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-01-11 21:46:44 +0530

    Packages

--- 15-loops.sh
+++ 15-loops.sh
@@ -1,46 +1,49 @@
 #!/bin/bash
-
 USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
 
-LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1 )
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+R= "\e[31m"
+G= "\e[32m"
+Y= "\e[33m"
+N= "\e[0m"
+
+LOGS_FOLDER= " /var/log/shell-script-logs"
+LOG_FILE=$(echo $0 | cut -d "." -fi)
+TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
+    if [ $? -ne 0]
     then
-        echo -e "$2 ... $R FAILURE $N"
-        exit 1
+      echo -e "$2.....$R Failure  $N"
+      exit 1
     else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
+      echo -e "$2.....$G Success $N"  
+    fi  
 }
 
-CHECK_ROOT(){
-    if [ $USERID -ne 0 ]
-    then
-        echo "ERROR:: You must have sudo access to execute this script"
-        exit 1 #other than 0
-    fi
-}
+    echo "Script started executing at :$TIMESTAMP " &>>$LOG_FILE_NAME
 
-echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+if [ $USERID -ne 0]
+then 
+  echo "ERROR :: u must have sudo sccess to executr this script"
+  exit 1
+fi
 
-CHECK_ROOT
 
 for package in $@
-do
-    dnf list installed $package &>>$LOG_FILE_NAME
-    if [ $? -ne 0 ]
+   do 
+     dnf list installed $package &>>$LOG_FILE_NAME
+    if [$? -ne 0]
     then
-        dnf install $package -y &>>$LOG_FILE_NAME
-        VALIDATE $? "Installing $package"
+        dnf install $package -y 
+        VALIDATE $? "Installing $package" &>>$LOG_FILE_NAME
     else
-        echo -e "$package is already $Y ... INSTALLED $N"
+     echo  -e "$package is already $y ........Installed $N"
     fi
-done
+
+done         
+
+
+  
+   
+
